57b5ed9a0061052463ba15d88f22a83d7f6618e6
BagusAdjieSatrio <90388947+BagusAdjieSatrio@users.noreply.github.com>
2023-10-21 11:42:55 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input</title>
+</head>
+<body>
+    <form>
+        <label for="inputAngka">Input Angka:</label>
+        <input type="number" id="inputAngka" name="inputAngka" placeholder="Masukkan angka" required>
+
+        <label for="inputDropdown">Pilih opsi:</label>
+        <select id="inputDropdown" name="inputDropdown">
+            <option value="option1">Opsi 1</option>
+            <option value="option2">Opsi 2</option>
+            <option value="option3">Opsi 3</option>
+        </select>
+    <button type="button" id="hitung">Hitung</button>
+    </form>
+</body>
+</html>
